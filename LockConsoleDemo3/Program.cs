7d8a33eea3dd4143419fca477765cdfdd0f1eed3
zhaijingfu <wenhui.zhai@elibchina.cn>
2021-07-09 11:08:05 +0800

    mutext and await

--- LockConsoleDemo3/Program.cs
+++ LockConsoleDemo3/Program.cs
@@ -8,6 +8,29 @@ namespace LockConsoleDemo3
     ///     优惠券的获取 每一个人都有一张优惠券 每个人都有自己的知识点以及等级进度
     /// 异步模型：
     ///     Async Await 报错
+    ///     Await在返回时有可能更改线程的ID,不能在asyc和await之间使用await
+    ///     
+    ///     注释 ReleaseMutex
+    ///     出现问题：
+    ///     Exception: Cannot access a disposed object. A common cause of this error is disposing a context that was resolved from 
+    ///     dependency injection and then later trying to use the same context instance elsewhere in your application. This may occur
+    ///     if you are calling Dispose() on the context, or wrapping the context in a using statement. If you are using dependency 
+    ///     injection, you should let the dependency injection container take care of disposing context instances.
+    ///     
+    ///     原因：
+    ///     In my case, i see the same behavior like Nathan Schubkegel. I use await's, and Thread.CurrentThread.ManagedThreadId gives
+    ///     another value for the "same" thread. I mean, thread was started with ManagedThreadId == 10, and Mutex was owned with this
+    ///     thread id, but later ReleaseMutex() causes ApplicationException with message: "Object synchronization method was called 
+    ///     from an unsynchronized block of code", and i see that ManagedThreadId == 11 at this time :) . It seems, await sometimes 
+    ///     changes
+    ///     thread id when returns. It seems, that is the reason. Mutex thinks that another thread wants to release it. It's sad, 
+    ///     that Mutex documentation does not make ATTENTION on this moment.
+    ///     So, you CAN NOT use asynchronous operator await between Mutex acquire and release.It's because C# compiler replaces 
+    ///     plain operator await by asynchronous callback, and this callback can be made by ANOTHER thread. Usually, it's the same 
+    ///     thread,but sometimes it's another thread (from thread pool). Mutex checks thread.Only thread that acquired Mutex may 
+    ///     release it. If you need synchronization without this checking, use Semaphore. SemaphoreSlim has asynchronous method 
+    ///     WaitAsync() - it's cool.
+    ///     
     /// </summary>
     class Program
     {
@@ -82,7 +105,7 @@ namespace LockConsoleDemo3
 
         static void LockGetCoupon(Person person)
         {
-            Console.WriteLine("date:{0:yyyy-MM-dd HH:mm:ss},name:{1},前来领取优惠券", DateTime.Now, person.Name); 
+            Console.WriteLine("date:{0:yyyy-MM-dd HH:mm:ss},name:{1},前来领取优惠券", DateTime.Now, person.Name);
             lock (LockObj)
             {
 
@@ -99,7 +122,7 @@ namespace LockConsoleDemo3
                     //领取
                     person.IsGetCoupon = true;
                     Console.WriteLine("date:{0:yyyy-MM-dd HH:mm:ss},name:{1},领取成功", DateTime.Now, person.Name);
-                } 
+                }
             }
         }
 
@@ -111,11 +134,11 @@ namespace LockConsoleDemo3
         {
             Console.WriteLine("date:{0:yyyy-MM-dd HH:mm:ss},name:{1},前来领取优惠券", DateTime.Now, person.Name);
 
-            using (var mutex = new Mutex(false,person.Id.ToString()))
+            using (var mutex = new Mutex(false, person.Id.ToString()))
             {
                 try
                 {
-                    if (mutex.WaitOne(-1,false))
+                    if (mutex.WaitOne(-1, false))
                     {
 
                         if (person.IsGetCoupon)
@@ -145,7 +168,7 @@ namespace LockConsoleDemo3
                 finally
                 {
                     mutex.ReleaseMutex();
-                    Console.WriteLine(person.Name.ToString()+"释放锁"+mutex.GetHashCode());
+                    Console.WriteLine(person.Name.ToString() + "释放锁" + mutex.GetHashCode());
                 }
             }
         }
